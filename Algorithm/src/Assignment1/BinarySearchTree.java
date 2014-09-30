08254b32e9002a807c451f7af70c657b52038b69
Dandan <dandan.wang1129@gmail.com>
2014-09-30 17:57:18 -0400

    Update BinarySearchTree.java

--- Algorithm/src/Assignment1/BinarySearchTree.java
+++ Algorithm/src/Assignment1/BinarySearchTree.java
@@ -1,16 +1,6 @@
 package Assignment1;
 
-class BinaryNode{
-	int val;
-	BinaryNode left;
-	BinaryNode right;
-	BinaryNode(int x){
-		val=x;
-		left=null;
-		right=null;
-	}
-}
-public class BinarySearchTree {
+classpublic class BinarySearchTree {
 	public void insert(int val){
 		
 	}
