07151cad0e35ce5ec72fb6c1d4d2be796b05cafa
Dandan <dandan.wang1129@gmail.com>
2014-09-30 17:29:18 -0400

    LinkedList class has two methods, find and insert

---
+++ Algorithm/src/Assignment1/BinarySearchTree.java
@@ -0,0 +1,21 @@
+package Assignment1;
+
+class BinaryNode{
+	int val;
+	BinaryNode left;
+	BinaryNode right;
+	BinaryNode(int x){
+		val=x;
+		left=null;
+		right=null;
+	}
+}
+public class BinarySearchTree {
+	public void insert(int val){
+		
+	}
+	
+	public boolean find(int val){
+		return false;
+	}
+}
---
+++ Algorithm/src/Assignment1/LinkedList.java
@@ -0,0 +1,49 @@
+package Assignment1;
+
+class ListNode{
+	int val;
+	ListNode next;
+	public ListNode(int x){
+		val=x;
+		next=null;
+	}
+}
+public class LinkedList {
+	ListNode dummy=new ListNode(0);
+	ListNode node=dummy;
+	
+	public void insert(int val){
+		//insert from the end,too slow
+	/*	node=dummy;
+		while(node.next!=null){
+			if(node.next.val==val)
+				break;
+			node=node.next;
+		}
+		node.next=new ListNode(val);*/
+		
+		
+		//insert form the head
+		node=dummy;
+		while(node.next!=null){
+			if(find(val))
+				break;
+			new ListNode(val).next=node;
+			node=new ListNode(val);
+			
+		}
+		node=new ListNode(val);
+		}
+	
+	
+	public boolean find(int val){
+		node=dummy;
+		while(node.next!=null){
+			if(node.next.val==val){
+				return true;
+			}
+			node=node.next;
+		}
+		return false;
+	}
+}
